08e672bcbd143afee7a9f9c7b2a07af3b2fa9e02
Daniel Lau <daniel.lau1@tesco.com>
2020-05-03 20:34:01 +0100

    React components practice

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDom from "react-dom";
+import CommentDetail from "./CommentDetail";
+import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const arrayOfComments = [
+  { author: "Sam", timeAgo: "Today at 6pm", text: "Nice Post" },
+  { author: "Daniel", timeAgo: "3 days ago", text: "Cool Story" },
+  { author: "Jenny", timeAgo: "Yesterday at 10pm", text: "This is so boring!" },
+];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      {arrayOfComments.map((comment, index) => {
+        return (
+          <ApprovalCard key={Math.floor(Math.random() * 999)}>
+            <CommentDetail
+              author={comment.author}
+              timeAgo={comment.timeAgo}
+              text={comment.text}
+              avatar={faker.image.avatar()}
+              key={index}
+            />
+          </ApprovalCard>
+        );
+      })}
+    </div>
+  );
+};
+
+ReactDom.render(<App />, document.querySelector("#root"));
